cc5d34a0f2e9e6dddae24d9d0f5998c93ac7d702
FesenkoE <fesenkoe@ukr.net>
2021-02-03 22:51:23 +0200

    task 3 is done

--- 01.02.2021/hw3.py
+++ 01.02.2021/hw3.py
@@ -101,3 +101,47 @@
 #         print('Numbers should be positive')
 # except ValueError:
 #     print('Invalid literal')
+
+
+"""
+    Магическое число.
+    Необходимо угадать загаданное число за наименьшее количество попыток.
+    Алгоритм:
+    1. Генерируется случайное число.
+    2. Пользователь вводит число.
+    3. Если введенное число больше или меньше сгенерированного, то
+        выводится соответствующее сообщение и возвращаемся к пункту 2.
+    4. Иначе введенное число равняется сгенерированному -
+        пользователь угадал число. Выводится само число и количество попыток.
+        А так же вопрос "Continue? (Y/n) ".
+    6. Если пользователь выбирает продолжить -
+        обнуляем счетчик попыток и переходим к пункту 1.
+    7. Иначе выводим сообщение 'Bye!'.
+    * Для генерации случайного числа используем random.randint(-inf, +inf),
+        где -inf - +inf - диапазон возможных чисел
+    ** по желанию, можете хранить рекордное число попыток
+    и сообщать пользователю, если он поставил новый рекорд
+"""
+
+# import random
+#
+# random_number = random.randint(1, 100)  # случайное число от 1 до 100
+#
+# var = True
+# attempt = 0
+#
+# while var:
+#     number = int(input('Enter a number: '))
+#     attempt += 1
+#
+#     if number < random_number:
+#         print('Number is less than Magic Number')
+#     elif number > random_number:
+#         print('Number is more than Magic Number')
+#     else:
+#         print('You guessed Magic Number: ', random_number, '\nYour attempt is: ', attempt)
+#         attempt = 0
+#
+#         answer = input('Continue? (Y/n): ')
+#         if answer == 'n':
+#             var = False
